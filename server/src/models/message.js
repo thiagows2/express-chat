936d2e08e970ef970bca31028d99615ebcb0fbb2
Thiago Marques <thiagomarques408@gmail.com>
2023-08-01 23:42:13 -0300

    fix: ajusta model Message

--- server/src/models/message.js
+++ server/src/models/message.js
@@ -4,21 +4,19 @@ const uuid = require('uuid')
 dynamoose.aws.ddb.local()
 
 const messageSchema = new dynamoose.Schema({
-    id: {
-      type: String,
-      hashKey: true,
-      default: uuid.v1()
-    },
-    user: {
-      type: String
-    },
-    message: {
-      type: String,
-      required: true
-    }
+  id: {
+    type: String,
+    hashKey: true,
+    default: uuid.v1()
   },
-  {
-    timestamps: true
-  })
+  user: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  }
+})
 
 module.exports = dynamoose.model('Message', messageSchema)
